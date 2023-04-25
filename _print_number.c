73edec2073705ad571d4b14aa8aa2e57bfd57b07
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-04-25 05:54:10 -0700

    more handlers

--- _print_number.c
+++ _print_number.c
@@ -24,7 +24,36 @@ int _print_d(va_list args)
 
 	if (n / 10)
 	{
-		count += _print_dec(n / 10);
+		count += _print_d(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+	count++;
+	return (count);
+}
+
+/**
+ * _print_i - prints given int
+ *
+ * @args: string to be printed
+ *
+ * Return: 1 denoting success
+ */
+int _print_i(va_list args)
+{
+	int n = va_arg(args, int);
+	int count = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		count++;
+	}
+
+	if (n / 10)
+	{
+		count += _print_i(n / 10);
 	}
 
 	_putchar((n % 10) + '0');
