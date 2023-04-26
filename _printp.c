ed8df766656e7929ccbf11f91648b6854d7465cc
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-04-25 19:16:38 -0700

    betty fix

--- _printp.c
+++ _printp.c
@@ -12,34 +12,34 @@
  */
 int _print_p(va_list args)
 {
-    void *p = va_arg(args, void *);
-    unsigned long int n = (unsigned long int)p;
-    int printed = 0;
+	void *p = va_arg(args, void *);
+	unsigned long int n = (unsigned long int)p;
+	int printed = 0;
 
-    printed += _putchar('0');
-    printed += _putchar('x');
+	printed += _putchar('0');
+	printed += _putchar('x');
 
-    if (n == 0)
-    {
-        printed += _putchar('0');
-    }
-    else
-    {
-        char hex[16] = "0123456789abcdef";
-        char buf[20];
-        int i = 0;
+	if (n == 0)
+	{
+		printed += _putchar('0');
+	}
+	else
+	{
+		char hex[16] = "0123456789abcdef";
+		char buf[20];
+		int i = 0;
 
-        while (n != 0)
-        {
-            buf[i++] = hex[n % 16];
-            n /= 16;
-        }
+		while (n != 0)
+		{
+			buf[i++] = hex[n % 16];
+			n /= 16;
+		}
 
-        while (i > 0)
-        {
-            printed += _putchar(buf[--i]);
-        }
-    }
+		while (i > 0)
+		{
+			printed += _putchar(buf[--i]);
+		}
+	}
 
-    return (printed);
+	return (printed);
 }
