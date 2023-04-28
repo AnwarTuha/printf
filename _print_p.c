830bb410da1f69891317fb3fcd02c5c797295384
AnwarTuha <anwartuha2@gmail.com>
2023-04-28 15:11:43 +0300

    minor fix

--- _print_p.c
+++ _print_p.c
@@ -12,7 +12,39 @@
  */
 int _print_p(va_list args)
 {
-	void *ptr = va_arg(args, void *);
+	void *p = va_arg(args, void *);
+	unsigned long int n = (unsigned long int)p;
+	int printed = 0;
 
-	return (printf("%p", ptr));
+	if (args == NULL)
+	{
+		return (printf("(nil)"));
+	}
+
+	printed += _putchar('0');
+	printed += _putchar('x');
+
+	if (n == 0)
+	{
+		printed += _putchar('0');
+	}
+	else
+	{
+		char hex[16] = "0123456789abcdef";
+		char buf[20];
+		int i = 0;
+
+		while (n != 0)
+		{
+			buf[i++] = hex[n % 16];
+			n /= 16;
+		}
+
+		while (i > 0)
+		{
+			printed += _putchar(buf[--i]);
+		}
+	}
+
+	return (printed);
 }
